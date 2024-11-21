40fda94d765567509f4ef388836824bb54f8b20a
Md Towhidul Alam <alammdtowhidul9@gmail.com>
2024-11-21 17:00:57 +0600

    add

--- pointOfSaleSpringBoot/src/main/java/com/towhid/practicepharmacy/service/TokenService.java
+++
@@ -1,8 +0,0 @@
-package com.towhid.practicepharmacy.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class TokenService {
-
-}
--- point_of_sale_flutter/android/app/src/main/java/com/towhid/point_of_sale/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.towhid.point_of_sale;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
